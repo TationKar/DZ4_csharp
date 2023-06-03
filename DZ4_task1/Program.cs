c5810e317a9f63c4d3c23b379df6c03551c6d07b
Timur Karimov <tationkar@gmail.com>
2023-06-03 10:31:25 +0300

    first

--- DZ4_task1/Program.cs
+++ DZ4_task1/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и 
+возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16 */
+Console.Clear();
+Console.Write("Введите число А: ");
+int numA = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите число B: ");
+int numB = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine($"Число {numA} возведенное в степень {numB} = {natDeg(numA, numB)}");
+
+
+int natDeg(int a, int b){
+int res = a;
+for (int i = 1; i < b; i++)
+    {
+        res *= a; 
+    }
+return res;
+}
+Console.WriteLine("Проверка " + Math.Pow(numA,numB));
